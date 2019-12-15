2e8e3f347154e93252b78ba3dc2a522b5112393e
GANI-A.M <gani131990@outlook.com>
2019-12-15 20:54:49 +0530

    JWT Config

---
+++ zuul-gateway/zuul-gateway/src/main/java/com/gri/tutorial/zuulgateway/service/MyUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.gri.tutorial.zuulgateway.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if ("gani".equals(username)) {
+            //password = "password
+            return new User("gani", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+                    new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+    }
+
+}
